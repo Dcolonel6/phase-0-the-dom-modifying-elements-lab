8e4ed5cc0bee3a4819a289161f18ae1a362adfc4
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-04 19:42:32 +0300

    manipulated the dom

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+function removeElement(element){
+    const ele = document.querySelector(element);
+    return ele.remove();
+}
+function createElementWithAttr(elementTag,attrObj){
+    const ele = document.createElement(elementTag);
+    ele.textContent = 'YOUR-NAME is the champion';
+    for(const key in attrObj){
+        ele.setAttribute(key,attrObj[key]);
+    }
+    return ele;
+
+}
+function appendElement(elementToAppendTo, elementToAppend){
+    elementToAppendTo.append(elementToAppend);
+
+    return elementToAppendTo;
+}
+const newHeader = createElementWithAttr('h1', {'id':'victory'});
+removeElement('main#main');
+
